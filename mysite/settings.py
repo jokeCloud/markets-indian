c68a1cac90562e70b59a63c3429e5de32c00ae91
johansen masterson <medalhaemerson@gmail.com>
2023-02-27 14:42:00 -0300

    strapi

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# STRIPE FAKE
+STRIPE_SECRET_KEY = '01234'
+STRIPE_PUBLISHABLE_KEY = '56789'
